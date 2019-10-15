557f345b6f600d0092a2e7a4582b4f17046fe6a7
Eric Erhardt <eric.erhardt@microsoft.com>
2019-10-15 14:24:26 -0500

    Initial test

---
+++ BreakingChangeTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary1;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using System;
+
+namespace BreakingChangeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Class1();
+            var c2 = new Class2<RegressionPredictionTransformer<LinearRegressionModelParameters>, LinearRegressionModelParameters>();
+
+            StaticMethods.TakeOneIn(c1);
+            StaticMethods.TakeOneIn(c2);
+
+            StaticMethods.TakeGenericOneIn(c1);
+            StaticMethods.TakeGenericOneIn(c2);
+
+            var r1 = StaticMethods.ReturnOne();
+            var r2 = StaticMethods.ReturnGenericOne<RegressionPredictionTransformer<LinearRegressionModelParameters>, LinearRegressionModelParameters>();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using System;
+
+namespace ClassLibrary1
+{
+    public class Class1 : ITrainerEstimator<RegressionPredictionTransformer<LinearRegressionModelParameters>, LinearRegressionModelParameters>
+    {
+        public TrainerInfo Info => null;
+
+        public RegressionPredictionTransformer<LinearRegressionModelParameters> Fit(IDataView input)
+        {
+            return null;
+        }
+
+        public SchemaShape GetOutputSchema(SchemaShape inputSchema)
+        {
+            return null;
+        }
+    }
+
+    public class Class2<TTransformer, TModel> : ITrainerEstimator<TTransformer, TModel>
+        where TTransformer : ISingleFeaturePredictionTransformer<TModel>
+        where TModel : class
+    {
+        public TrainerInfo Info => null;
+
+        public TTransformer Fit(IDataView input)
+        {
+            return default;
+        }
+
+        public SchemaShape GetOutputSchema(SchemaShape inputSchema)
+        {
+            return null;
+        }
+    }
+
+    public static class StaticMethods
+    {
+        public static void TakeOneIn(ITrainerEstimator<RegressionPredictionTransformer<LinearRegressionModelParameters>, LinearRegressionModelParameters> trainerEstimator)
+        {
+            Console.WriteLine(nameof(TakeOneIn));
+        }
+
+        public static ITrainerEstimator<RegressionPredictionTransformer<LinearRegressionModelParameters>, LinearRegressionModelParameters>  ReturnOne()
+        {
+            Console.WriteLine(nameof(ReturnOne));
+            return null;
+        }
+
+        public static void TakeGenericOneIn<TTransformer, TModel>(ITrainerEstimator<TTransformer, TModel> trainerEstimator)
+            where TTransformer : ISingleFeaturePredictionTransformer<TModel>
+            where TModel : class
+        {
+            Console.WriteLine(nameof(TakeGenericOneIn));
+        }
+
+        public static ITrainerEstimator<TTransformer, TModel> ReturnGenericOne<TTransformer, TModel>()
+            where TTransformer : ISingleFeaturePredictionTransformer<TModel>
+            where TModel : class
+        {
+            Console.WriteLine(nameof(ReturnGenericOne));
+            return null;
+        }
+    }
+}
